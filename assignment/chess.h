2f597d8f7b5c04cdde249a3f0eb87c9d915ca21f
Alexander Handstanger <zombeyfan.ah@gmail.com>
2018-10-25 13:36:39 +0200

    Sirst Steps

--- assignment/chess.h
+++ assignment/chess.h
@@ -1,12 +1,33 @@
 /*----------------------------------------------------------
- *				HTBLA-Leonding / Klasse: 2AHDV
+ *				HTBLA-Leonding / Klasse: 2DHIF
  * ---------------------------------------------------------
- * Exercise Number: 0
+ * Exercise Number: 6
  * Title:			chess.h
- * Author:			P. Bauer
+ * Author:			A. Handstanger
  * Due Date:		October 15, 2014
  * ----------------------------------------------------------
  * Description:
  * Basic chess functions.
  * ----------------------------------------------------------
  */
+
+enum Color {White, Black};
+enum PieceType{
+  PAWN, ROOK, KNIGHT, BISHOP, QUEEN, KING
+};
+
+struct ChessPiece{
+  enum Color color;
+  enum PieceType type;
+};
+
+struct ChessSquare{
+  bool is_occupied;
+  struct ChessPiece piece;
+};
+
+typedef ChessSquare ChessBoard[8] [8];
+
+bool is_piece(struct ChessPiece piece, enum Color color, enum PieceType type);
+
+void init_chess_board(ChessBoard chess_board);
