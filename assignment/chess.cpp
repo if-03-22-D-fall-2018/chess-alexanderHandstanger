f13c0ed6ef67d2fad709bde0f5282311936492f7
alexanderHandstanger <zombeyfan.ah@gmail.com>
2018-11-05 19:05:17 +0000

    empty functions

--- assignment/chess.cpp
+++ assignment/chess.cpp
@@ -36,3 +36,51 @@ struct ChessSquare* get_square(ChessBoard chess_board, File file, Rank rank){
   }
   return &chess_board[rank][file];;
 }
+
+bool is_square_occupied(ChessBoard chess_board, File file, Rank rank){
+
+}
+
+bool add_piece(ChessBoard chess_board, File file, Rank rank, struct ChessPiece piece){
+
+}
+
+struct ChessPiece get_piece(ChessBoard chess_board, File file, Rank rank){
+
+}
+
+void setup_chess_board(ChessBoard chess_board){
+
+}
+
+bool remove_piece(ChessBoard chess_board, File file, Rank rank){
+
+}
+
+bool squares_share_file(File file, Rank rank, File file1, Rank rank1){
+
+}
+
+bool squares_share_rank(File file, Rank rank, File file1, Rank rank1){
+
+}
+
+bool squares_share_diagonal(File file, Rank rank, File file1, Rank rank1){
+
+}
+
+bool squares_share_kings_move(File file, Rank rank, File file1, Rank rank1){
+
+}
+
+bool squares_share_pawns_move(enum Color color, enum Move NormalMove, File file, Rank rank, File file1, Rank rank1){
+
+}
+
+bool squares_share_queens_move(File file, Rank rank, File file1, Rank rank1){
+
+}
+
+bool squares_share_knights_move(File file, Rank rank, File file1, Rank rank1){
+
+}
